635175425a2d11a8a0ff99ef4369ae060da8247e
Ambre Austen Suhamy <ambre@tarides.com>
2023-10-31 11:10:13 +0100

    Added a lot of different variants

--- theories/Definitions.v
+++ theories/Definitions.v
@@ -1,21 +1,134 @@
-From Coq Require Export String.
-From Coq Require Export Ascii.
+From Coq Require Import String.
+From Coq Require Import Ascii.
+
+Inductive prefix_1 : string -> string -> Prop :=
+| P1_Empty : forall s2, prefix_1 "" s2
+| P1_S : forall s1 s2, prefix_1 s1 s2 -> forall c, prefix_1 (String c s1) (String c s2)
+.
+
+Inductive prefix_2 : string -> string -> Prop :=
+| P2_Empty : forall s2, prefix_2 "" s2
+| P2_S : forall s1 s2 c, prefix_2 (String c s1) (String c s2) -> prefix_2 s1 s2
+.
+
+
+Inductive sub_1 : string -> string -> Prop :=
+| S1 : forall pre post s1, sub_1 s1 (pre ++ s1 ++ post)%string
+.
+
+Inductive sub_2 : string -> string -> Prop :=
+| S2_Prefix : forall n h, prefix_1 n h -> sub_2 n h
+| S2_pN : forall n h c, sub_2 (String c n) h -> sub_2 n h
+.
+
+Inductive sub_3 : string -> string -> Prop :=
+| S3_Prefix : forall n h, prefix_1 n h -> sub_3 n h
+| S3_sH : forall n h, sub_3 n h -> forall c, sub_3 n (String c h)
+.
+
+Inductive sub_4 : string -> string -> Prop :=
+| S4 : forall s1 s2 pre post, s2 = (pre ++ s1 ++ post)%string -> sub_4 s1 s2
+.
+
+Fixpoint sub_5 s1 s2 :=
+  match s1, s2 with
+  | EmptyString, _ => true
+  | _, EmptyString => false
+  | _, String _ s2' => (prefix s1 s2 || sub_5 s1 s2')%bool
+  end.
+
+Fixpoint sub_6 s1 s2 :=
+  match s1, s2 with
+  | EmptyString, _ => True
+  | _, EmptyString => False
+  | _, String _ s2' => prefix s1 s2 = true \/ sub_6 s1 s2'
+  end.
+
+Fixpoint sub_7 s1 s2 :=
+  match s2 with
+  | EmptyString => s1 = EmptyString
+  | String _ s2' => prefix s1 s2 = true \/ sub_7 s1 s2'
+  end.
+
+Fixpoint sub_optimized (s1 s2 : string) :=
+  (* Needs to be extractable to OCaml *) True
+.
+
+Theorem correct : forall s1 s2, sub_optimized s1 s2 <-> sub_? s1 s2.
+Proof.
+Admitted.
+
 
 Fixpoint is_substring needle haystack :=
-  prefix needle haystack = true \/
   match haystack with
-  | EmptyString => False
-  | String _ h' => is_substring needle h'
+  | EmptyString => needle = EmptyString
+  | String _ h =>
+    prefix needle haystack = true \/
+    is_substring needle h
   end.
 
-Fixpoint is_found_loop pos needle haystack :=
+Lemma is_sub_body : forall needle haystack, is_substring needle haystack =
   match haystack with
-  | EmptyString => None
-  | String _ h' =>
-    if prefix needle haystack then
-      Some pos
-    else
-      is_found_loop (S pos) needle h'
+  | EmptyString => needle = EmptyString
+  | String _ h =>
+    prefix needle haystack = true \/
+    is_substring needle h
+  end.
+Proof.
+  now destruct needle, haystack.
+Qed.
+
+Fixpoint is_found_at needle haystack pos :=
+  if prefix needle haystack then
+    exists i, pos = Some i /\ i <= length haystack
+  else
+    match haystack with
+    | EmptyString => pos = None
+    | String _ h => exists i, pos = Some (S i) /\ is_found_at needle h (Some i)
+    end.
+
+
+Lemma found_body : forall needle haystack pos, is_found_at needle haystack pos =
+  if prefix needle haystack then
+    exists i, pos = Some i /\ i <= length haystack
+  else
+    match haystack with
+    | EmptyString => pos = None
+    | String _ h => exists i, pos = Some (S i) /\ is_found_at needle h (Some i)
+    end.
+Proof.
+  now destruct needle, haystack.
+Qed.
+
+Fixpoint found_2 needle haystack pos :=
+  match pos with
+  | Some 0 => prefix needle haystack = true
+  | Some (S i) =>
+    match haystack with
+    | EmptyString => False
+    | String _ h => found_2 needle h (Some i)
+    end
+  | None =>
+    match haystack with
+    | EmptyString => True
+    | String _ h => found_2 needle h None
+    end
   end.
 
-Definition is_found_at := is_found_loop 0.
+Lemma found_body_2 : forall needle haystack pos, found_2 needle haystack pos =
+  match pos with
+  | Some 0 => prefix needle haystack = true
+  | Some (S i) =>
+    match haystack with
+    | EmptyString => False
+    | String _ h => found_2 needle h (Some i)
+    end
+  | None =>
+    match haystack with
+    | EmptyString => True
+    | String _ h => found_2 needle h None
+    end
+  end.
+Proof.
+  now destruct needle, haystack.
+Qed.
--- theories/Theorems.v
+++ theories/Theorems.v
@@ -1,9 +1,7 @@
-(* begin hide *)
 From Coq Require Export String.
 From Coq Require Export Ascii.
 
 From Evidence Require Import Definitions.
-(* end hide *)
 
 
 Lemma prefix_body: forall s1 s2, prefix s1 s2 =
@@ -25,23 +23,57 @@ Proof.
   apply prefix_body.
 Qed.
 
-Lemma is_sub_body: forall needle haystack,
-  is_substring needle haystack =
-  (prefix needle haystack = true \/
-    match haystack with
-    | EmptyString => False
-    | String _ h' => is_substring needle h'
-    end).
-Proof.
-  unfold is_substring.
-  now destruct needle; destruct haystack.
-Qed.
-
-Example is_sub_trivial : forall s: string, is_substring EmptyString s.
+Lemma prefix_S : forall s1 s2, prefix s1 s2 = true ->
+                 forall c, prefix (String c s1) (String c s2) = true.
 Proof.
   intros.
-  rewrite is_sub_body; left.
-  now rewrite prefix_body.
+  rewrite prefix_body.
+  induction (ascii_dec c c); easy.
+Qed.
+
+Lemma prefix_P : forall s1 s2 c1 c2, prefix (String c1 s1) (String c2 s2) = true -> c1 = c2 /\ prefix s1 s2 = true.
+Proof.
+  intros.
+  rewrite prefix_body in H.
+  induction (ascii_dec c1 c2); easy.
+Qed.
+
+Lemma is_sub_empty : forall s: string, is_substring EmptyString s.
+Proof.
+  induction s; simpl; auto.
+Qed.
+
+Theorem found_bounded : forall n h i, found_2 n h (Some i) ->
+                        i <= length h.
+Proof.
+  induction n, h, i; intros; try now apply le_0_n.
+  - now rewrite found_body_2 in H.
+  - rewrite found_body_2 in H.
+  induction (prefix n h).
+  - destruct H as [j [H1 H2]].
+    now inversion H1; subst.
+  - destruct h.
+    + inversion H.
+    +
+Admitted.
+
+Theorem opt_Some_eq : forall {A : Type} (x y : A), Some x = Some y <-> x = y.
+Proof.
+  split;
+  intros;
+  now inversion H.
+Qed.
+
+Lemma found_empty : forall h i, is_found_at EmptyString h (Some i) ->
+                    i <= length h.
+Proof.
+  induction h; intros.
+  - simpl in *.
+    destruct H as [j [H1 H2]].
+    
+      rewrite found_body in H.
+  - simpl.
+    now exists i.
 Qed.
 
 Example is_sub_foo : is_substring "hi" "machine".
@@ -53,6 +85,12 @@ Proof.
   now left.
 Qed.
 
+Example found_at_x : is_found_at "hi" "machine" (Some 3).
+Proof.
+  simpl.
+  repeat eexists.
+Qed.
+
 Lemma is_sub_S_h : forall n h, is_substring n h ->
                    forall c, is_substring n (String c h).
 Proof.
@@ -61,44 +99,61 @@ Proof.
   now right.
 Qed.
 
+Lemma found_S_n : forall n h c i, is_found_at (String c n) h (Some i) ->
+                  is_found_at n h (Some (S i)).
+Proof.
+  induction n, h; intros; try easy.
+  - apply found_empty.
+
+Lemma found_S_h : forall n h i, is_found_at n h (Some i) ->
+                  forall c, is_found_at n (String c h) (Some (S i)).
+Proof.
+  intros.
+  induction n, h; intros.
+  - simpl.
+    now exists (S i).
+  - simpl.
+    now exists (S i).
+  - inversion H.
+  - admit.
+Admitted.
+  
+
 Lemma is_sub_P_n : forall n h c, is_substring (String c n) h ->
                    is_substring n h.
 Proof.
-  induction h; intros.
-  - rewrite is_sub_body in H.
-    destruct H; inversion H.
-  - induction (ascii_dec c a).
-    + subst a.
-      rewrite is_sub_body in H.
-      induction H.
-      * rewrite is_sub_body.
-        right.
-        rewrite is_sub_body.
-        left.
-        rewrite prefix_body in H.
-        now induction (ascii_dec c c).
-      * apply IHh in H.
-        now apply is_sub_S_h.
-    + rewrite is_sub_body in H.
-      induction H.
-      * rewrite prefix_body in H.
-        now induction (ascii_dec c a).
-      * apply IHh in H.
-        now apply is_sub_S_h.
+  induction h; try easy.
+  intros.
+  induction H.
+  - apply prefix_P in H.
+    destruct H; subst.
+    rewrite is_sub_body.
+    right.
+    rewrite is_sub_body.
+    destruct h.
+    + rewrite prefix_body in H0.
+      now induction n.
+    + now left.
+  - eapply is_sub_S_h, IHh, H.
 Qed.
 
-Lemma is_sub_both : forall n h c,
-                      is_substring (String c n) (String c h)
+Lemma is_sub_both : forall n h c1 c2,
+                      is_substring (String c1 n) (String c2 h)
                    -> is_substring n h.
 Proof.
-  intros.
-  rewrite is_sub_body in H.
-  induction H.
+  induction h; intros.
   - rewrite is_sub_body.
-    left.
-    rewrite prefix_body in H.
-    now induction (ascii_dec c c).
-  - now apply (is_sub_P_n _ _ c).
+    rewrite is_sub_body in H.
+    destruct H.
+    + apply prefix_P in H.
+      now induction n.
+    + now induction n.
+  - rewrite is_sub_body in H.
+    destruct H.
+    + apply prefix_P in H as [_ H].
+      rewrite is_sub_body.
+      now left.
+    + now apply is_sub_P_n with c1.
 Qed.
 
 Theorem longer_sub : forall n h, is_substring n h -> length n <= length h.
@@ -106,19 +161,77 @@ Proof.
   induction n; induction h; intros;
     simpl;
     try apply le_0_n.
-  - induction H; inversion H.
+  - inversion H.
   - apply le_n_S.
     apply IHn.
-    induction H.
-    + rewrite prefix_body in H.
-      induction (ascii_dec a a0).
-      * rewrite is_sub_body.
-        now left.
-      * inversion H.
-    + now apply is_sub_P_n in H.
+    now apply is_sub_both in H.
 Qed.
 
-Example found_at_x : is_found_at "hi" "machine" = Some 3.
+
+
+Theorem sub_found_eq : forall h n, is_substring n h <-> exists i, is_found_at n h (Some i).
 Proof.
-  reflexivity.
-Qed.
+  split;
+  intros.
+  - rewrite is_sub_body in H.
+    induction h.
+    + exists 0.
+      now subst.
+    + destruct H.
+      * exists 0.
+        rewrite found_body.
+        now rewrite H.
+      * rewrite <- is_sub_body in IHh.
+        apply IHh in H as I.
+        destruct I as [i I].
+        simpl.
+        induction n.
+        -- now exists 0.
+        -- destruct IHn.
+           ++ now apply is_sub_P_n with a0.
+           ++ intros.
+              
+        rewrite found_body.
+        symmetry.
+        
+        
+        
+
+        
+      
+      
+  eexists.
+    rewrite found_body.
+    induction (prefix n h).
+    + reflexivity.
+    + 
+
+
+
+
+
+  induction h; split; intros.
+  - exists 0.
+    inversion H; subst.
+    now simpl.
+  - simpl in *.
+    induction H.
+    now destruct n.
+  - destruct IHh with n as [H_left H_right].
+    
+    rewrite found_body.
+    induction (prefix n (String a h)).
+    + trivial.
+    + induction h.
+      * inversion H.
+      * admit.
+  -
+    simpl.
+    induction (ascii_dec a a0); subst.
+    + induction (prefix n h).
+      * now exists 0.
+      * 
+  apply is_sub_both in H.
+
+    apply IHn.
+   
